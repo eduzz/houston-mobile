de5468ce1ab22ef263b4b6b2c5572b5ffb4fabd0
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-08-03 15:12:36 -0300

    feat: add function to scale differents screens

---
+++ src/helpers/screenScale.ts
@@ -0,0 +1,12 @@
+import { Dimensions, PixelRatio, Platform } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const IS_ANDROID = Platform.OS === 'android';
+
+// based on iphone 5s's scale
+const scale = SCREEN_WIDTH > 700 ? 1.4 : SCREEN_WIDTH / 320;
+
+export function screenScale(size: number) {
+  const newSize = size * (IS_ANDROID ? scale * 0.9 : scale * 0.95);
+  return Math.round(PixelRatio.roundToNearestPixel(newSize));
+}
