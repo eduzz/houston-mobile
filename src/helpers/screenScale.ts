e1715dc6da0250f617712d2c776f0bf7f433b2ab
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-08-03 23:25:16 -0300

    chore: fix default function

--- src/helpers/screenScale.ts
+++ src/helpers/screenScale.ts
@@ -6,7 +6,7 @@ const IS_ANDROID = Platform.OS === 'android';
 // based on iphone 5s's scale
 const scale = SCREEN_WIDTH > 700 ? 1.4 : SCREEN_WIDTH / 320;
 
-export function screenScale(size: number) {
+export default function screenScale(size: number) {
   const newSize = size * (IS_ANDROID ? scale * 0.9 : scale * 0.95);
   return Math.round(PixelRatio.roundToNearestPixel(newSize));
 }
