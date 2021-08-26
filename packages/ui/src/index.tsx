c44f146aee760ed9c6e6ff5ce0f1cafaa2628d5f
danilolucasmd <danilolucasmd@gmail.com>
2021-08-26 11:21:16 -0300

    feat: add react-native-elements and export all its components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,3 +3,5 @@ import ActionItem from "./ActionSheet/ActionItem";
 import Modal, { IModal } from "./Modal";
 
 export { Modal, IModal, ActionSheet, ActionItem };
+
+export * from "react-native-elements";
