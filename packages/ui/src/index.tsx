589ecdb214b9fdf799613310650e92b1914c297d
danilolucasmd <danilolucasmd@gmail.com>
2021-08-28 16:06:48 -0300

    feat: export MaterialIcons component from react-native-vector-icons in the Icons package

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,9 +1,10 @@
 import SearchBar from "react-native-platform-searchbar";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 
 import ActionSheet from "./ActionSheet";
 import ActionItem from "./ActionSheet/ActionItem";
 import Modal, { IModal } from "./Modal";
 
-export { Modal, IModal, ActionSheet, ActionItem, SearchBar };
+export { Modal, IModal, ActionSheet, ActionItem, SearchBar, MaterialIcon };
 
 export * from "native-base";
