ec7f2f5f61b905b532bac4986d0a5f8d5f6a6206
danilolucasmd <danilolucasmd@gmail.com>
2021-08-28 15:54:44 -0300

    feat: add react-native-platform-searchbar as dependency

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,7 +1,9 @@
+import SearchBar from "react-native-platform-searchbar";
+
 import ActionSheet from "./ActionSheet";
 import ActionItem from "./ActionSheet/ActionItem";
 import Modal, { IModal } from "./Modal";
 
-export { Modal, IModal, ActionSheet, ActionItem };
+export { Modal, IModal, ActionSheet, ActionItem, SearchBar };
 
 export * from "native-base";
